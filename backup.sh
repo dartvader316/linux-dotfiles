b2f0c1360196d3b55466ecef71ef5038f1f442bd
dartvader316 <dartvader316@tutanota.com>
2019-10-27 15:22:00 +0300

    Another upd 2

--- backup.sh
+++ backup.sh
@@ -1,6 +1,7 @@
 cp $HOME/.bashrc ./home/.bashrc
 cp $HOME/.xinitrc ./home/.xinitrc
 cp $HOME/.Xresources ./home/.Xresources
+cp $HOME/.vimrc ./home/.vimrc
 cp $HOME/.config/compton/* ./home/.config/compton/
 cp $HOME/.config/i3/* ./home/.config/i3/
 cp $HOME/.config/polybar/* ./home/.config/polybar/
---
+++ home/.vimrc
@@ -0,0 +1,11 @@
+set number	
+set linebreak	 
+set showbreak=+++
+set textwidth=100	
+set visualbell
+set t_Co=256
+syntax on
+colorscheme deus
+hi NonText ctermbg=none 
+hi Normal guibg=NONE ctermbg=NONE 
+imap ;; <Esc>               
